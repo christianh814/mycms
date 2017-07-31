b59880a97820c3fdf19c081bb1992ac7e00dc672
root <christian.hernandez@yahoo.com>
2017-07-31 14:11:00 -0700

    another big bag of changes

--- index.php
+++ index.php
@@ -12,6 +12,18 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
+		<?php
+		$query = "SELECT * FROM posts";
+		$select_all_posts = mysqli_query($connect, $query);
+		while ($post = mysqli_fetch_assoc($select_all_posts)) {
+			$post_title = $post['post_title'];
+			$post_author = $post['post_author'];
+			$post_date = $post['post_date'];
+			$post_image = $post['post_image'];
+			$post_content = $post['post_content'];
+		// Breakout of php here for HTML below
+		?>
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -19,17 +31,19 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+		<a href="#"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+		<?php } // this closes the `while` loop ?>
 
             </div>
 

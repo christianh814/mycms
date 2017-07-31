3d9618d73b50df843a2922911f9bd703c90c7eef
root <christian.hernandez@yahoo.com>
2017-07-31 15:30:16 -0700

    Now we tell you your browser sucks in your native tongue.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,56 +4,47 @@
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
+		    <form action="search.php" method="post">
                     <div class="input-group">
-                        <input type="text" class="form-control">
+                        <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
                     </div>
+		    </form>
                     <!-- /.input-group -->
                 </div>
 
                 <!-- Blog Categories Well -->
                 <div class="well">
+                <?php
+                        $query = "SELECT * FROM categories";
+                        $select_sidebar_categories = mysqli_query($connect, $query);
+
+                ?>
+
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+				<?php 
+					while ($cat_sidebar = mysqli_fetch_assoc($select_sidebar_categories)) {
+                                		$cat_sidebar_title = $cat_sidebar['cat_title'];
+                                		echo "<li><a href='#'>" . $cat_sidebar_title . "</a></li>";
+                        		}
+				?>
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
+
+
                     </div>
                     <!-- /.row -->
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+		<?php include "widget.php"; ?>
 
             </div>
 
---
+++ includes/widget.php
@@ -0,0 +1,4 @@
+<div class="well">
+  <h4>Side Widget Well</h4>
+  <p>This is just a placeholder for furture development!</p>
+</div>
